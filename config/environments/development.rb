fba99891a6635c5ccf70495d0120f0dadedf3e80
yyamnk <y.yamnk@gmail.com>
2015-04-04 02:18:26 +0900

    e-mailを設定, deviceでパスワードリセット用メール送信を確認

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,30 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  #
+
+  # ---------------------------
+  # for device, e-mail
+  # ---------------------------
+  # sample settings
+  # https://github.com/RailsApps/rails-devise/blob/master/config/environments/development.rb
+  # `Rails.application.secrets.[domain_name, email_username, email_password]`は
+  # `config/secrets.yml`で定義される (このとき環境変数を読み込むべき)
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: Rails.application.secrets.domain_name,
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: Rails.application.secrets.email_username,
+      password: Rails.application.secrets.email_password
+    }
+    # ActionMailer Config
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.raise_delivery_errors = true
+    # Send email in development mode?
+    config.action_mailer.perform_deliveries = true
+
+
 end
