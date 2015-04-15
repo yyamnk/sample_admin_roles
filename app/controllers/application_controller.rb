75c4561553d45570425f9a57bfd4d54c602230f2
yyamnk <y.yamnk@gmail.com>
2015-04-16 02:16:05 +0900

    add: welcome controller, view, deviseでログイン後に特定のページヘ遷移させた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #サインイン後の遷移先
+  def after_sign_in_path_for(resource)
+    welcome_index_path
+    # rake routesの<prefix>_pathで飛ぶ.
+    # root_pathはダメだった.
+  end
+
+  #ログアウト後の遷移先
+  def after_sign_out_path_for(resource)
+    admin_root_path
+  end
 end
