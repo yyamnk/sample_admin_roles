2235aed282760a345f8cb0c0598ae098d2d7e34b
yyamnk <y.yamnk@gmail.com>
2015-04-26 19:32:37 +0900

    fix: views/welcome/index, 登録画面へのリンクを修正

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,7 @@ class WelcomeController < ApplicationController
     @user = current_user # deviseでログインしているユーザを取得
 
     # UserDetail.find( id )だと存在しな時にエラー吐く. nilを渡すようにする
-    @user_detail = UserDetail.find_by(id: current_user.id)
+    @user_detail = UserDetail.find_by(user_id: current_user.id)
+
   end
 end
