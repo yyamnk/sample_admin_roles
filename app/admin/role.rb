11fc118c3a7d97307f34ad29f15e10890b916f57
yyamnk <y.yamnk@gmail.com>
2015-04-28 01:38:45 +0900

    mod: Roleモデル修正, バリデート追加, admin/roleでpermit_params追加

--- app/admin/role.rb
+++ app/admin/role.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Role do
 
+  actions :all, :except => [:destroy] # destory以外はOKにする
+  permit_params :name
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,7 @@
 class Role < ActiveRecord::Base
   has_many :users
+
+  validates :name,
+    presence: true,  # 必須
+    uniqueness: true # 重複不可
 end
